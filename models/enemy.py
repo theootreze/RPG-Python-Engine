b5e9c42c4a301f97debfb6c2836e7ed58fa6991a
Theo Treze! <theo13enrico@gmail.com>
2026-04-02 00:56:18 -0300

    add Enemy model with basic attributes

---
+++ models/enemy.py
@@ -0,0 +1,12 @@
+class Enemy:
+    def __init__(self, name = 'Monster', hp = 10, attack = 10):
+        self.name = name
+        self.hp = hp
+        self.attack = attack
+    
+    def take_damage(self, amount):
+        self.hp -= amount
+    
+    def heal(self, amount):
+        self.hp += amount
+        
